6fd45a597ebc0aedef562eda555f606dcd0dc958
Max Strübing <mxstrbng@gmail.com>
2018-07-13 18:26:12 +0200

    add style to button

--- assets/elm/Components/Button.elm
+++ assets/elm/Components/Button.elm
@@ -1,9 +1,18 @@
 module Components.Button exposing (button)
 
 import Html exposing (Attribute, Html)
+import Html.Attributes exposing (style)
+
+
+buttonStyle : Attribute msg
+buttonStyle =
+    style
+        [ ( "backgroundColor", "red" )
+        , ( "height", "90px" )
+        , ( "width", "100%" )
+        ]
 
 
 button : List (Attribute msg) -> List (Html msg) -> Html msg
 button attributes contents =
-    -- use Html.button because otherwise it would be a recursive call
-    Html.button attributes contents
+    Html.button (buttonStyle :: attributes) contents
